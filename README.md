dbbe1f43790c227f4694aa64bb4a9acf9889a875
Katelyn Slater <ktaeyln@gmail.com>
2014-10-14 23:30:51 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+[![Build Status](https://travis-ci.org/NexusTools/node-nhp.svg)](https://travis-ci.org/NexusTools/node-nhp) [![Apache License 2.0](http://img.shields.io/hexpm/l/plug.svg)](https://coveralls.io/r/NexusTools/node-typeinclude) [![Coverage Status](https://img.shields.io/coveralls/NexusTools/node-nhp.svg)](https://coveralls.io/r/NexusTools/node-nhp) [![Gratipay Tips](http://img.shields.io/gratipay/ktaeyln.svg)](https://gratipay.com/ktaeyln/)
+
+node-nhp
+--------
+node-nhp is a html templating language which uses tags similar to php, but a javascript like language syntax.
+
+legal
+-----
+node-typeinclude is licensed under [Apache License 2.0](LICENSE.md)
---
+++ index.ts
@@ -0,0 +1,26 @@
+import expat = require("node-expat");
+import fs = require("fs");
+
+class NHP {
+    private globals:Object = {};
+    
+    public compile(input:String, output:String, callback:Function) {
+        console.log("Compiling", input);
+        
+        var parser = new expat.Parser('UTF-8');
+        
+        parser.on("processingInstruction", function(target, data) {
+            console.log(target, data);
+        });
+        parser.on("error", function(error) {
+            callback(error);
+        });
+        parser.on("end", function() {
+            callback();
+        });
+
+        fs.createReadStream(input).pipe(parser);
+    }
+}
+
+module.exports = NHP;
