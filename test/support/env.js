9f0c9de619847e9c0aabacedcd1d03bdf725e8c0
Katelyn Slater <ktaeyln@gmail.com>
2014-10-21 03:15:47 -0400

    Ensure NODE_PATH is correct for tests

--- test/support/env.js
+++ test/support/env.js
@@ -1,2 +1,6 @@
+var path = require("path");
+
 process.env.NODE_ENV = 'test';
+process.env.NODE_PATH = path.dirname(path.dirname(__dirname)) + path.sep + "node_modules:" + process.env.NODE_PATH;
+console.log(process.env.NODE_PATH);
 //process.env.TYPEINCLUDE_VERBOSE = true;
