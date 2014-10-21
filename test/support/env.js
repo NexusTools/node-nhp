2f3f13cf87286fad4a4256083aab165ca08bd542
Katelyn Slater <ktaeyln@gmail.com>
2014-10-21 04:38:12 -0400

    Cleaned environment

--- test/support/env.js
+++ test/support/env.js
@@ -1,6 +1,1 @@
-var path = require("path");
-
 process.env.NODE_ENV = 'test';
-process.env.NODE_PATH = path.dirname(path.dirname(__dirname)) + path.sep + "node_modules:" + process.env.NODE_PATH;
-console.log(process.env.NODE_PATH);
-//process.env.TYPEINCLUDE_VERBOSE = true;
